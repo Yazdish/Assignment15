35a9d3a10d2c04ce27d63172785e8422895ac393
Yazdan Shouryabi <128619328+Yazdish@users.noreply.github.com>
2023-10-16 11:52:26 +0330

    Add files via upload

--- fruit.py
+++ fruit.py
@@ -8,22 +8,22 @@ class Fruit(arcade.Sprite):
         super().__init__()
         self.width = 32
         self.height = 32
-        self.center_x = random.randint(10, game.width - 10)
-        self.center_y = random.randint(10, game.height - 10)
+        self.center_x = random.randint(16, game.width - 16)
+        self.center_y = random.randint(16, game.height - 16)
         self.change_x = 0
         self.change_y = 0
 
 class Apple(Fruit):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__("apple.png")
         self.value = 1
 
 class Pear(Fruit):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__("pear.png")
         self.value = 2
 
 class Poop(Fruit):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__("poop.png")
         self.value = -1
